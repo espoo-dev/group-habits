f544e6640e0ea567f00d38a5b5303116aebce194
Iury Nogueira <iunog@hotmail.com>
2022-11-07 21:14:40 -0300

    :sparkles: Homescreen and Habit component

---
+++ svelte/src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Habit from "./lib/components/Habit.svelte";
+</script>
+
+<main class="text-center page-container">
+
+  <nav>
+    Today
+  </nav>
+
+  <Habit />
+</main>
+
+<style>
+  :global(body) {
+    background-color: #151515;
+    color: #fff;
+  }
+  .page-container {
+    padding: 8px;
+  }
+  nav {
+    padding: 8px;
+    color: #eaeaea;
+    text-align: left;
+
+    font-size: 22px;
+    font-weight: bold
+  }
+</style>
---
+++ svelte/src/lib/components/Habit.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  const item = {
+    label: "Item",
+    icone: "a",
+    selected: true
+  }
+
+  console.log('item -> ', item);
+</script>
+
+<div class="container-item">
+  <div class="icon-habit">
+    <div class="icon-box">
+      <i class="fa-regular fa-heart" style="color: purple;"></i>
+    </div>
+    <span class="habit-name">Eat 2 fruits</span>
+  </div>
+
+  <input type="checkbox" name="item" id="item">
+</div>
+
+<style>
+  .habit-name {
+    margin-left: 8px;
+  }
+
+  .icon-habit {
+    display: flex;
+    align-items: center;
+  }
+
+  .container-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .icon-box {
+    background: #231f1d;
+    padding: 8px;
+    border-radius: 8px;
+  }
+</style>
