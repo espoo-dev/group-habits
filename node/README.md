eef5626853e14f6e13570a04521d009556ef50e5
Chrislayne <84159430+chrislaynexavier@users.noreply.github.com>
2022-11-08 19:00:17 -0300

    create project node (#7)
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>

---
+++ node/README.md
@@ -0,0 +1,5 @@
+# group-habits
+
+Node version: 16.18.0
+
+`yarn install`
---
+++ node/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
+  moduleNameMapper: {
+    '@/test/(.+)': '<rootDir>/test/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.test.ts'],
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/test'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
+}
