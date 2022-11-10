8ce10990431fe5e39d6c36780ba941a06c53581a
chrislaynexavier <chrislayne_pereira@hotmail.com>
2022-11-10 19:31:18 -0300

    add daily habit to habit

--- node/test/Habit.spec.ts
+++ node/test/Habit.spec.ts
@@ -1,8 +1,23 @@
+import { DailyHabit } from "../src/domain/entity/DailyHabit";
 import { Habit } from "../src/domain/entity/Habit";
 
 describe('Habit', () => {
+  let newHabit!: Habit
+  let nameHabit!: string
+  let dailyHabit!: DailyHabit
+  let date = new Date()	
+
+  beforeEach(() => {
+    newHabit = new Habit(nameHabit)
+    dailyHabit = new DailyHabit(date, newHabit)
+  })
+
   it('should have a name', () => {
-    let newHabit = new Habit('drink water')
-    expect(newHabit.name).toBe('drink water')
+    expect(newHabit.name).toBe(nameHabit)
   });
+
+  it('should have daily habit', () => {
+    newHabit.addDailyHabit(dailyHabit)
+    expect(newHabit.dailiesHabits).toContain(dailyHabit)
+  })
 });
