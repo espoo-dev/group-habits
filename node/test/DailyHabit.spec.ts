3216045010e18db150b7b3d03af0873b8b7e3ee6
chrislaynexavier <chrislayne_pereira@hotmail.com>
2022-11-10 18:09:36 -0300

    refactor daily habit test

--- node/test/DailyHabit.spec.ts
+++ node/test/DailyHabit.spec.ts
@@ -2,14 +2,15 @@ import { DailyHabit } from "../src/domain/entity/DailyHabit"
 
 describe('daily Habit', () => {
   let newDailyHabit!: DailyHabit
+  let date = new Date()
+
   beforeEach(() => {
-    newDailyHabit = new DailyHabit(new Date())
+    newDailyHabit = new DailyHabit(date)
   })
 
-  it('should have day', () => {
-    expect(newDailyHabit.date.getDate()).toBe(new Date().getDate())
-    expect(newDailyHabit.date.getMonth()).toBe(new Date().getMonth())
-    expect(newDailyHabit.date.getFullYear()).toBe(new Date().getFullYear())
+  it('should have a date', () => {
+    expect(newDailyHabit.date).toBe(date)
+    expect(newDailyHabit.date).toBeInstanceOf(Date)
   })
 
   it('should habit start with check false', () => {
@@ -21,7 +22,7 @@ describe('daily Habit', () => {
     expect(newDailyHabit.checked).toBe(true)
   } )
 
-  it('should be check false', () => {
+  it('should have checked equals to false when toggle twice', () => {
     newDailyHabit.toggleCheck()
     newDailyHabit.toggleCheck()
     expect(newDailyHabit.checked).toBe(false)
