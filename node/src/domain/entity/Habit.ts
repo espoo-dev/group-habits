398e6739a80a4e5d98d8d83c355ec392f440ecd1
Chrislayne <84159430+chrislaynexavier@users.noreply.github.com>
2022-11-16 19:43:02 -0300

    Add Daily Habit to Habit (#23)
    Co-authored-by: Edimo Silva <edimossilva@gmail.com>

--- node/src/domain/entity/Habit.ts
+++ node/src/domain/entity/Habit.ts
@@ -1,7 +1,15 @@
+import { DailyHabit } from "./DailyHabit"
+
 export class Habit {
   public name!: string
+  public dailiesHabits: DailyHabit[] = []
 
   constructor(name: string) {
     this.name = name
+  }
+
+  addDailyHabit(dailyHabit: DailyHabit) {
+    this.dailiesHabits.push(dailyHabit)
+    dailyHabit.habit = this
   }
 }
--- node/test/Habit.spec.ts
+++ node/test/Habit.spec.ts
@@ -1,9 +1,28 @@
+import { DailyHabit } from "../src/domain/entity/DailyHabit";
 import { Habit } from "../src/domain/entity/Habit";
 
 describe('Habit', () => {
+  let newHabit!: Habit
+  let nameHabit!: string
+  let dailyHabit!: DailyHabit
+  let date = new Date()	
+
+  beforeEach(() => {
+    newHabit = new Habit(nameHabit)
+    dailyHabit = new DailyHabit(date)
+  })
+
   it('should have a name', () => {
-    const habitName = 'drink water'
-    let newHabit = new Habit(habitName)
-    expect(newHabit.name).toBe(habitName)
-  });
-});
+    expect(newHabit.name).toBe(nameHabit)
+  })
+
+  it('should contain the new daily habit added in habit', () => {
+    newHabit.addDailyHabit(dailyHabit)
+    expect(newHabit.dailiesHabits).toContain(dailyHabit)
+  })
+
+  it('should daily habit has a habit', () => {
+    newHabit.addDailyHabit(dailyHabit)
+    expect(dailyHabit.habit).toBe(newHabit)
+  })
+})
