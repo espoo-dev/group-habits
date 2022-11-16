25b1b032d287cc175e40e89f4553b46e467eb65b
Chrislayne <84159430+chrislaynexavier@users.noreply.github.com>
2022-11-16 19:11:05 -0300

    Update node/test/Habit.spec.ts
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>

--- node/test/Habit.spec.ts
+++ node/test/Habit.spec.ts
@@ -2,7 +2,8 @@ import { Habit } from "../src/domain/entity/Habit";
 
 describe('Habit', () => {
   it('should have a name', () => {
-    let newHabit = new Habit('drink water')
-    expect(newHabit.name).toBe('drink water')
+    const habitName = 'drink water'
+    let newHabit = new Habit(habitName)
+    expect(newHabit.name).toBe(habitName)
   });
 });
