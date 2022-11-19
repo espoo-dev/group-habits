64b6e2726cbe51aca25ee90110816debf31db9c6
Edimo Silva <edimossilva@gmail.com>
2022-11-19 16:35:43 -0300

    44 - improve specs

--- backend-rails/spec/models/habit_spec.rb
+++ backend-rails/spec/models/habit_spec.rb
@@ -10,20 +10,7 @@
 require 'rails_helper'
 
 RSpec.describe Habit, type: :model do
-  let(:habit) { build(:habit) }
-  let(:incomplete_habit) { build(:habit, name: '') }
-
-  describe '#valid?' do
-    context 'when Habit has a name' do
-      it 'returns true' do
-        expect(habit.valid?).to eq true
-      end
-    end
-
-    context 'when Habit does not have a name' do
-      it 'returns false' do
-        expect(incomplete_habit.valid?).to eq false
-      end
-    end
+  describe 'validates' do
+    it { should validate_presence_of(:name) }
   end
 end
