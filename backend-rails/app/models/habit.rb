3ee1d282f3ecf8c212bb5705c64ea6c0db9e863e
Edimo Silva <edimossilva@gmail.com>
2022-11-19 21:45:10 -0300

    43 - add field icon to habit (#46)

--- backend-rails/app/models/habit.rb
+++ backend-rails/app/models/habit.rb
@@ -6,6 +6,7 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  icon       :string
 #
 class Habit < ApplicationRecord
   validates :name, presence: true
