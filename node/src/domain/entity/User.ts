d4da729a50d2ad86802aec01df4c98caacdf2cb3
Chrislayne <84159430+chrislaynexavier@users.noreply.github.com>
2022-11-22 19:44:49 -0300

    Update node/src/domain/entity/User.ts
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>

--- node/src/domain/entity/User.ts
+++ node/src/domain/entity/User.ts
@@ -3,7 +3,7 @@ import { DailyHabit } from "./DailyHabit"
 export class User {
     public email!: string
     public password!: string
-    public dailiesHabits:DailyHabit[] = []
+    public dailiesHabits: DailyHabit[] = []
     constructor(email: string, password: string){
         this.email = email
         this.password = password
