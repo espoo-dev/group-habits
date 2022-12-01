aef3f68f11d5db2ab0f2840092e6f0d4d18482e3
chrislaynexavier <chrislayne_pereira@hotmail.com>
2022-12-01 18:16:37 -0300

    revolve conflicts

--- node/test/User.spec.ts
+++ node/test/User.spec.ts
@@ -13,25 +13,6 @@ describe('User', () => {
       dailyHabit = new DailyHabit(date)
     })
 
-    it('should have a e-mail', () => {
-      expect(newUser.email).toBe(email)
-    })
-
-    it('should have a password', () => {
-      expect(newUser.password).toBe(password)
-    })
-
-    it('should contain the new daily habit added in User', () => {
-      newUser.addDailyHabit(dailyHabit)
-      expect(newUser.dailiesHabits).toContain(dailyHabit)
-    })
-
-    it('should daily habit has a user', () => {
-      newUser.addDailyHabit(dailyHabit)
-      expect(dailyHabit.user).toBe(newUser)
-      dailyHabit = new DailyHabit(date)
-    })
-
     it('should have a user with correct e-mail', () => {
       expect(newUser.email).toBe(email)
     })
@@ -43,5 +24,10 @@ describe('User', () => {
     it('should contain the new daily habit added in User', () => {
       newUser.addDailyHabit(dailyHabit)
       expect(newUser.dailiesHabits).toContain(dailyHabit)
+    })
+    
+    it('should daily habit has a user', () => {
+      newUser.addDailyHabit(dailyHabit)
+      expect(dailyHabit.user).toBe(newUser)
     })
 })
