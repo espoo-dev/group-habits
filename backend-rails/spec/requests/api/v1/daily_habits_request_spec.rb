5b92af410009f26097ee415b3a10c5403bd0ee0e
Edimo Silva <edimossilva@gmail.com>
2022-12-03 17:54:21 -0300

    80 - generate missing daily_habits

--- backend-rails/spec/requests/api/v1/daily_habits_request_spec.rb
+++ backend-rails/spec/requests/api/v1/daily_habits_request_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 describe 'api/v1/daily_habits', type: :request do
   describe '#index' do
-    let(:user) { create(:user) }
-    let!(:daily_habits) { create_list(:daily_habit, 2, user:) }
-    let!(:daily_habits2) { create_list(:daily_habit, 3) }
+    let(:user) { create(:user, group:) }
+    let(:group) { create(:group) }
+    let!(:habit) { create(:habit, group:) }
+    let!(:daily_habits) { create_list(:daily_habit, 2, user:, group:) }
+    let!(:daily_habits2) { create_list(:daily_habit, 3, group:) }
 
     before { get api_v1_daily_habits_path, headers: auth_headers, as: :json }
     it 'returns status 200 ok' do
@@ -12,7 +14,7 @@ describe 'api/v1/daily_habits', type: :request do
     end
 
     it "returns user's daily_habits" do
-      expect(json_response.pluck('id')).to match_array(daily_habits.pluck(:id))
+      expect(json_response.pluck('id')).to match_array(daily_habits.pluck(:id) + [nil])
     end
   end
 end
