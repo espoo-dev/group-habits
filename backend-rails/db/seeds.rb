5c9b464c6c1bd538133992dd8d19557c0503a6f7
Edimo Silva <edimossilva@gmail.com>
2022-12-06 17:24:18 -0300

    fix seeds

--- backend-rails/db/seeds.rb
+++ backend-rails/db/seeds.rb
@@ -1,10 +1,10 @@
 if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password')
   group = Group.create!(name: 'Espoo lovers')
-  habit1 = Habit.create!(name: 'Drink water', group:)
-  habit2 = Habit.create!(name: 'Exercise', group:)
-  habit3 = Habit.create!(name: 'Eat 2 fruits', group:)
-  user = User.create!(email: 'user@email.com', password: '123456789', group:)
+  Habit.create!(name: 'Drink water', group:)
+  Habit.create!(name: 'Exercise', group:)
+  Habit.create!(name: 'Eat 2 fruits', group:)
+  User.create!(email: 'user@email.com', password: '123456789', group:)
 
   Setting.create_or_find_by!(key: 'min_version', value: '0.0')
 end
