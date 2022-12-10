d85b7a2c11be34093a3b1d6dc21f1008e1452075
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2022-12-10 19:26:53 -0300

    Created VerticalSpacer

---
+++ android-client/app/src/main/java/com/example/habits/presentation/components/VerticalSpacer.kt
@@ -0,0 +1,15 @@
+package com.example.habits.presentation.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+/**
+ * This is a custom [Spacer] that consumes a [height] and removes boilerplate.
+ */
+@Composable
+fun VerticalSpacer(height: Dp) {
+    Spacer(modifier = Modifier.height(height))
+}
