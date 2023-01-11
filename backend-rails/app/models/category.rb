5233a9a508fb063c75ee6591db73d0f7e95cf768
Edimo Silva <edimossilva@gmail.com>
2023-01-11 20:22:14 -0300

    97: annotate category

--- backend-rails/app/models/category.rb
+++ backend-rails/app/models/category.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_user_id           (user_id)
+#  index_categories_on_user_id_and_name  (user_id,name) UNIQUE
+#
 class Category < ApplicationRecord
   belongs_to :user
 
