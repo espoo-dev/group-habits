6baa334a1280089b098829c71b3caed25ab9848e
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2023-01-11 23:27:06 -0300

    fix lint

--- android-so/app/src/main/java/com/group/so/domain/di/DomainModule.kt
+++ android-so/app/src/main/java/com/group/so/domain/di/DomainModule.kt
@@ -1,8 +1,6 @@
 package com.group.so.domain.di
 
-
 import com.group.so.domain.LoginUseCase
-
 import org.koin.core.context.loadKoinModules
 import org.koin.core.module.Module
 import org.koin.dsl.module
