22d97e647a203b3333b9be44176c8721adaf0243
Edimo Silva <edimossilva@gmail.com>
2023-01-12 21:44:45 -0300

    102: Add category presenter

---
+++ backend-rails/app/presenters/category_presenter.rb
@@ -0,0 +1,16 @@
+class CategoryPresenter
+  attr_accessor :category
+
+  def initialize(category)
+    @category = category
+
+    super()
+  end
+
+  def payload
+    {
+      id: category.id,
+      name: category.name,
+    }
+  end
+end
---
+++ backend-rails/spec/presenters/category_presenter_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CategoryPresenter do
+  describe '#payload' do
+    let!(:category) { create(:category) }
+    subject { described_class.new(category).payload }
+
+    it 'matches expected attributes' do
+      expected_payload = {
+        id: category.id,
+        name: category.name,
+      }
+
+      is_expected.to eq expected_payload
+    end
+  end
+end
