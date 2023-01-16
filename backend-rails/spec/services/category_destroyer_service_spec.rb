3c13406d5e0459b438ee9334906207eaac9f814f
Edimo Silva <edimossilva@gmail.com>
2023-01-16 17:30:37 -0300

    112: create category destroyer service

---
+++ backend-rails/spec/services/category_destroyer_service_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CategoryDestroyerService do
+  describe '#call' do
+    let!(:user) { create(:user) }
+    let!(:destroy_category_params) { {id: category.id} }
+
+    subject { described_class.new(user:, destroy_category_params:).call }
+
+    describe 'when user is category owner' do
+      let!(:category) { create(:category, user:) }
+      it 'destroys category' do
+        expect { subject }.to change { Category.count }.from(1).to(0)
+      end
+    end
+
+    describe 'when user is not category owner' do
+      let!(:category) { create(:category) }
+      it 'destroys category' do
+        expect { subject }.to raise_error Pundit::NotAuthorizedError
+      end
+    end
+  end
+end
