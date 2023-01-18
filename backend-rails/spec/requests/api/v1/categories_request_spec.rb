b5542bb9aee8d3de47f96f0f51cbf3d2aa202a63
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2023-01-17 23:16:51 -0300

    adds tests wip

--- backend-rails/spec/requests/api/v1/categories_request_spec.rb
+++ backend-rails/spec/requests/api/v1/categories_request_spec.rb
@@ -78,4 +78,71 @@ describe 'api/v1/categories', type: :request do
       end
     end
   end
+
+  require 'rails_helper'
+
+describe 'api/v1/categories', type: :request do
+  describe '#index' do
+    let(:user) { create(:user, categories: user_categories) }
+    let!(:user_categories) { create_list(:category, 1) }
+    let!(:categories2) { create_list(:category, 1) }
+
+    before { get api_v1_categories_path, headers: auth_headers, as: :json }
+
+    it 'returns status 200 ok' do
+      expect(response).to be_successful
+    end
+
+    it "returns user's categories" do
+      expect(json_response.pluck('id')).to match_array(user_categories.pluck(:id))
+    end
+  end
+
+  describe '#create' do
+    let(:user) { create(:user) }
+    let(:create_category_params) { attributes_for(:category, user_id: nil) }
+
+    before do
+      post api_v1_categories_path, params: create_category_params, headers: auth_headers, as: :json
+    end
+
+    it 'returns status 201 created' do
+      expect(response).to be_created
+    end
+
+    it 'returns category' do
+      expect(json_response['id']).to_not be_nil
+      expect(json_response['name']).to eq(create_category_params[:name])
+    end
+  end
+
+  describe '#destroy' do
+    let(:user) { create(:user) }
+    let!(:category) { create(:category, user:) }
+
+    before { delete api_v1_category_path(category.id), headers: auth_headers, as: :json }
+
+    it 'returns status 204 no_content' do
+      expect(response).to be_no_content
+    end
+
+    it 'returns an empty hash' do
+      expect(response.body).to eq('')
+    end
+  end
+
+  # describe '#update' do
+  #   let(:user) { create(:user) }
+  #   let!(:category) { create(:category, user:) }
+
+  #   before { put api_v1_category_path(category.id), headers: auth_headers, as: :json }
+
+  #   it 'returns status 200 ok' do
+  #     expect(response).to be_successful
+  #   end
+
+  #   it 'returns status 422' do
+  #     expect(response).to be_
+  #   end
+  # end
 end
---
+++ backend-rails/spec/services/category_finder_service_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CategoryFinderService do
+  describe '#call' do
+    # let!(:user) { create(:user) }
+    # let!(:update_category_params) { { id: category.id } }
+
+    # subject { described_class.new(user:, update_category_params:).call }
+
+    # describe 'when user is category owner' do
+    #   let!(:category) { create(:category, user:) }
+    #   it 'updates category' do
+    #     expect { subject }.to change { Category.name }
+    #   end
+    # end
+
+    # describe 'when user is not category owner' do
+    #   let!(:category) { create(:category) }
+    #   it 'updates category' do
+    #     expect { subject }.to raise_error Pundit::NotAuthorizedError
+    #   end
+    end
+  end
+end
