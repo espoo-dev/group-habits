000638599a0acc792ad16f499515453b3d7c1cc1
Edimo Silva <edimossilva@gmail.com>
2023-01-19 20:54:27 -0300

    fixes for CategoryUpdaterService

--- backend-rails/app/services/category_updater_service.rb
+++ backend-rails/app/services/category_updater_service.rb
@@ -1,17 +1,18 @@
 class CategoryUpdaterService
-  attr_reader :user, :id
+  attr_reader :user, :id, :name
 
-  def initialize(user:, id:)
+  def initialize(user:, update_category_params:)
     @user = user
-    @id = id
+    @id = update_category_params[:id]
+    @name = update_category_params[:name]
   end
 
   def call
-    category = Category.find_by(id: id, user_id: user.id)
-    
-    authorize!(user: user, category: category)
-    
-    category.update!(name: name)
+    category = Category.find(id)
+
+    authorize!(user:, category:)
+
+    category.update!(name:)
     category
   end
 
