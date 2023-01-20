f9c512c18ae90a95a74574c3f557468cf15db985
Edimo Silva <edimossilva@gmail.com>
2023-01-20 16:31:45 -0300

    121: filter at categories finder service

--- backend-rails/spec/services/categories_finder_service_spec.rb
+++ backend-rails/spec/services/categories_finder_service_spec.rb
@@ -5,13 +5,22 @@ require 'rails_helper'
 RSpec.describe CategoriesFinderService do
   describe '#call' do
     let!(:user) { create(:user) }
-    let!(:category) { create(:category, user:) }
-    let!(:category2) { create(:category) }
+    let!(:category) { create(:category, name: 'abc', user:) }
+    let!(:category2) { create(:category, name: 'def', user:) }
+    let!(:category3) { create(:category) }
 
-    subject { described_class.new(user:).call }
+    context 'when not querying' do
+      subject { described_class.new(user:, find_params: {}).call }
+      it 'returns user categories' do
+        is_expected.to eq [category, category2]
+      end
+    end
 
-    it 'returns user categories expected attributes' do
-      is_expected.to eq [category]
+    context 'when querying' do
+      subject { described_class.new(user:, find_params: { name: 'b' }).call }
+      it 'returns user categories matching filter' do
+        is_expected.to eq [category]
+      end
     end
   end
 end
