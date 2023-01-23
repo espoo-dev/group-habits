42eea562efb7d9f59cc3610e9ecd4cba16e49b6f
Edimo Silva <edimossilva@gmail.com>
2023-01-23 17:27:40 -0300

    130: add seed

--- backend-rails/db/seeds.rb
+++ backend-rails/db/seeds.rb
@@ -5,6 +5,15 @@ if Rails.env.development?
   Habit.create!(name: 'Exercise', group:)
   Habit.create!(name: 'Eat 2 fruits', group:)
   user = User.create!(email: 'user@email.com', password: '123456789', group:)
-  Category.create!(name: 'Home', user:)
+  category = Category.create!(name: 'Home', user:)
+  Item.create!(name: 'product 1', extra_info: 'info 1', sale_price: 11, purchase_price: 5,
+               sales_unit: 'unit', item_type: 'product', user:, category:)
+  Item.create!(name: 'product 2', extra_info: 'info 2', sale_price: 11, purchase_price: 5,
+               sales_unit: 'unit', item_type: 'product', user:, category:)
+  Item.create!(name: 'service 1', extra_info: 'info 2', sale_price: 11, purchase_price: 5,
+               sales_unit: 'unit', item_type: 'service', user:, category:)
+  Item.create!(name: 'service 2', extra_info: 'info 2', sale_price: 11, purchase_price: 5,
+               sales_unit: 'unit', item_type: 'service', user:, category:)
+
   Setting.create_or_find_by!(key: 'min_version', value: '0.0')
 end
