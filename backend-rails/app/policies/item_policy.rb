10442ccbd3884310726fa820bf0e318bc84d1b50
Edimo Silva <edimossilva@gmail.com>
2023-01-23 13:29:48 -0300

    128: create index to item

---
+++ backend-rails/app/policies/item_policy.rb
@@ -0,0 +1,18 @@
+class ItemPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+  def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      Item.where(user:)
+    end
+  end
+end
---
+++ backend-rails/spec/factories/items.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_user_id           (user_id)
+#  index_categories_on_user_id_and_name  (user_id,name) UNIQUE
+#
+FactoryBot.define do
+  factory :item do
+    name { 'laptop' }
+    extra_info { 'nice laptop' }
+    sale_price { 2000.00 }
+    purchase_price { 1500.55 }
+    sales_unit { "unit" }
+    item_type { "product" }
+
+    association :user
+    association :category
+  end
+end
