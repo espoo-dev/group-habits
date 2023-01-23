a49e073beed8c941901d6721dd18c49b2628b1bb
Edimo Silva <edimossilva@gmail.com>
2023-01-23 17:08:40 -0300

    130: add item_type param to query at ItemsFinderService

--- backend-rails/app/services/items_finder_service.rb
+++ backend-rails/app/services/items_finder_service.rb
@@ -1,14 +1,16 @@
 class ItemsFinderService < BaseService
-  attr_reader :user, :name
+  attr_reader :user, :name, :item_type
 
   def initialize(user:, index_params:)
     @user = user
     @name = index_params[:name]
+    @item_type = index_params[:item_type]
   end
 
   def call
     items = ItemPolicy::Scope.new(user, Item).resolve
                              .by_name_like(name)
+                             .by_item_type(item_type)
 
     authorize!(ItemPolicy, :index?, items)
 
