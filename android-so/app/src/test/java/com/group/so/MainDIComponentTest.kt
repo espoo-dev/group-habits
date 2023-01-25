5b44b46fe5fd29a57bb3b4a11dbd84a332ec3756
@Roanderson <roanderson.pinheiro@zig.fun>
2023-01-25 15:52:56 -0300

    chore: Change url BASE

--- android-so/app/src/test/java/com/group/so/MainDIComponentTest.kt
+++ android-so/app/src/test/java/com/group/so/MainDIComponentTest.kt
@@ -3,11 +3,12 @@ package com.group.so
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
 
-const val BASE_URL = "localhost:3000/api/v1/"
+const val BASE_URL = "https:3000/api/v1/"
 
 fun configureTestAppComponent() = startKoin {
+
     loadKoinModules(
         configureDataModuleForTest(BASE_URL) + configureDomainModuleForTest() +
-            configurePresentationModuleForTest() + configureDAOModuleForTest()
+                configurePresentationModuleForTest() + configureDAOModuleForTest()
     )
 }
