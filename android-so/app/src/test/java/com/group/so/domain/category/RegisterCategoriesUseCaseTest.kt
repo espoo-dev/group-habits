57a67872e89dc66260df36b51f561a6075a89938
@Roanderson <roanderson.pinheiro@zig.fun>
2023-01-25 15:52:17 -0300

    chore: Added RegisterCategoriesUseCaseTest.kt

---
+++ android-so/app/src/test/java/com/group/so/domain/category/RegisterCategoriesUseCaseTest.kt
@@ -0,0 +1,56 @@
+package com.group.so.domain.category
+
+import com.group.so.core.RemoteException
+import com.group.so.core.Resource
+import com.group.so.data.entities.request.CategoryDataRequest
+import com.group.so.data.repository.category.CategoryRepository
+import com.group.so.mock.CategoryMock
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+
+@RunWith(JUnit4::class)
+class RegisterCategoriesUseCaseTest {
+    private val categoryRepository = mockk<CategoryRepository>()
+    val registerCategoryUseCase = RegisterCategoryUseCase(categoryRepository)
+    val mockRegisterCategoryRequest =
+        CategoryDataRequest("category 1")
+
+
+    @Test
+    fun `should return a category after register`() =
+        runBlocking {
+
+            // GIVEN
+            coEvery {
+                categoryRepository.register(
+                    mockRegisterCategoryRequest
+                )
+            } returns CategoryMock.mockCategorRegisterResourceSucess()
+
+            val result = registerCategoryUseCase.execute(mockRegisterCategoryRequest).first()
+
+            // THEN
+            Assert.assertEquals(
+                result.data?.name,
+                CategoryMock.mockCategorRegisterResourceSucess().first().data?.name
+            )
+            Assert.assertTrue(result is Resource.Success)
+
+        }
+
+
+    @Test(expected = RemoteException::class)
+    fun `should throw an exception after trying to register a category`() = runBlocking {
+
+        coEvery { categoryRepository.register(mockRegisterCategoryRequest) } throws RemoteException("")
+        val result = registerCategoryUseCase(mockRegisterCategoryRequest)
+    }
+
+}
