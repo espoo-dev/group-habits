0c5751c38e4374e1390d358bffe27358ded81b8e
Edimo Silva <edimossilva@gmail.com>
2023-01-26 11:42:00 -0300

    152: add create customer endpoint

--- backend-rails/app/controllers/api/v1/customers_controller.rb
+++ backend-rails/app/controllers/api/v1/customers_controller.rb
@@ -2,14 +2,23 @@ module Api
   module V1
     class CustomersController < Api::V1::ApiController
       def index
-        items = CustomersFinderService.new(user: current_user, index_params:).call
-        render json: CustomerPresenter.payload_for_list(items)
+        customers = CustomersFinderService.new(user: current_user, index_params:).call
+        render json: CustomerPresenter.payload_for_list(customers)
+      end
+
+      def create
+        customer = CustomerCreatorService.new(user: current_user, create_customer_params:).call
+        render json: CustomerPresenter.payload_for_item(customer), status: :created
       end
 
       private
 
       def index_params
         params.permit(:name, :customer_type)
+      end
+
+      def create_customer_params
+        params.permit(:name, :document_number, :phone, :state_inscription, :customer_type)
       end
     end
   end
