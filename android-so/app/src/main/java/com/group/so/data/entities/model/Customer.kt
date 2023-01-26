7d086a670897cec9b12b6e455785d862a55d93e6
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2023-01-26 15:20:08 -0300

    Create client entities (#157)
    Co-authored-by: @Roanderson <roanderson.pinheiro@zig.fun>

---
+++ android-so/app/src/main/java/com/group/so/data/entities/model/Customer.kt
@@ -0,0 +1,10 @@
+package com.group.so.data.entities.model
+
+class Customer(
+    val id: Int,
+    val name: String,
+    val documentNumber: String,
+    val phone: String,
+    val stateInscription: String,
+    val customerType: String,
+)
---
+++ android-so/app/src/main/java/com/group/so/data/entities/network/CustomerDTO.kt
@@ -0,0 +1,46 @@
+package com.group.so.data.entities.network
+
+import com.group.so.data.entities.db.CustomerDb
+import com.group.so.data.entities.model.Customer
+import com.squareup.moshi.Json
+
+data class CustomerDTO(
+    val id: Int,
+    val name: String,
+    @field:Json(name = "document_number")
+    val documentNumber: String,
+    val phone: String,
+    @field:Json(name = "state_inscription")
+    val stateInscription: String,
+    @field:Json(name = "customer_type")
+    val customeType: String
+
+) {
+    fun toModel(): Customer = Customer(
+        id = 1,
+        name = name,
+        documentNumber = documentNumber,
+        phone = phone,
+        stateInscription = stateInscription,
+        customerType = customeType
+    )
+
+    fun toDb(): CustomerDb = CustomerDb(
+        id = 1,
+        name = name,
+        documentNumber = documentNumber,
+        phone = phone,
+        stateInscription = stateInscription,
+        customerType = customeType
+    )
+}
+
+fun List<CustomerDTO>.toModel(): List<Customer> =
+    this.map {
+        it.toModel()
+    }
+
+fun List<CustomerDTO>.toDb(): List<CustomerDb> =
+    this.map {
+        it.toDb()
+    }
