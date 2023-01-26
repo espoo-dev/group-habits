a7d5d4ebbbfb40197c32d8f65e1b13955079e241
@Roanderson <roanderson.pinheiro@zig.fun>
2023-01-26 14:48:57 -0300

    chore: Created entity CustomerDb

---
+++ android-so/app/src/main/java/com/group/so/data/entities/db/CustomerDb.kt
@@ -0,0 +1,32 @@
+package com.group.so.data.entities.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.group.so.data.entities.model.Customer
+
+@Entity(tableName = "customer")
+data class CustomerDb(
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+    val documentNumber: String,
+    val phone: String,
+    val stateInscription: String,
+    val customerType: String,
+
+) {
+
+    fun toModel(): Customer = Customer(
+        id = id,
+        name = name,
+        documentNumber = documentNumber,
+        phone = phone,
+        stateInscription = stateInscription,
+        customerType = customerType
+    )
+}
+
+fun List<CustomerDb>.toModel(): List<Customer> =
+    this.map {
+        it.toModel()
+    }
