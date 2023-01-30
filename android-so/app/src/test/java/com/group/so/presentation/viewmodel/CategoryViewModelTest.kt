3fa281e9011e452ebccbffa218210529e1db7473
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2023-01-30 13:49:42 -0300

    Add coveralls (#180)
    Co-authored-by: @Roanderson <roanderson.pinheiro@zig.fun>

--- android-so/app/src/test/java/com/group/so/presentation/viewmodel/CategoryViewModelTest.kt
+++ android-so/app/src/test/java/com/group/so/presentation/viewmodel/CategoryViewModelTest.kt
@@ -2,7 +2,8 @@
 
 package com.group.so.presentation.viewmodel
 
-import android.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.group.so.core.RemoteException
 import com.group.so.core.Resource
 import com.group.so.core.State
 import com.group.so.data.entities.model.Category
@@ -89,6 +90,20 @@ class CategoryViewModelTest {
         categoryState.value = viewModel.categoryState.value
 
         assert(categoryState.value is State.Success)
+    }
+
+    @Test
+    fun ` loading categories error `() = runTest {
+
+        val categoryState = MutableStateFlow<State<List<Category>>>(State.Idle)
+        coEvery { getCategoriesUseCase.execute() } returns flow {
+            emit(Resource.Error(data = null, error = RemoteException("")))
+        }
+        viewModel.fetchLatestCategories()
+        runCurrent()
+        categoryState.value = viewModel.categoryState.value
+
+        assert(categoryState.value is State.Error)
     }
 
     @Test
