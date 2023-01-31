112cdc758d71846c9cc0e6f90d00e78f7a6fd1bd
@Roanderson <roanderson.pinheiro@zig.fun>
2023-01-31 17:09:41 -0300

    chore: Update version db

--- android-so/app/src/main/java/com/group/so/data/database/ServiceOrderDatabase.kt
+++ android-so/app/src/main/java/com/group/so/data/database/ServiceOrderDatabase.kt
@@ -11,7 +11,7 @@ import com.group.so.data.entities.db.CustomerDb
 
 @Database(
     entities = [CategoryDb::class, CustomerDb::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class ServiceOrderDatabase : RoomDatabase() {
