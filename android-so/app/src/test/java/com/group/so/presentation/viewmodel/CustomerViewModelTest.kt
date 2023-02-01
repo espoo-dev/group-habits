29183ff072433736b424ab5add0162486d4960a4
@Roanderson <roanderson.pinheiro@zig.fun>
2023-02-01 16:38:43 -0300

    test: Created test loading customers successfully

---
+++ android-so/app/src/test/java/com/group/so/presentation/viewmodel/CustomerViewModelTest.kt
@@ -0,0 +1,84 @@
+@file:Suppress("MaxLineLength", "FunctionParameterNaming", "FunctionNaming", "LongParameterList")
+
+package com.group.so.presentation.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.group.so.core.Resource
+import com.group.so.core.State
+import com.group.so.data.entities.model.Customer
+import com.group.so.data.repository.customer.CustomerRepository
+import com.group.so.domain.customer.GetCustomersByCustomTypeUseCase
+import com.group.so.domain.customer.GetCustomersByNameUseCase
+import com.group.so.domain.customer.GetCustomersUseCase
+import com.group.so.mock.CustomerMock
+import com.group.so.presentation.ui.customer.CustomerViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class CustomerViewModelTest {
+
+    val customerRepository = mockk<CustomerRepository>(relaxed = true)
+
+    val getCustomersUseCase = GetCustomersUseCase(customerRepository)
+
+    val getCustomersByCustomTypeUseCase = GetCustomersByCustomTypeUseCase(customerRepository)
+
+    val getCustomersByNameUseCase = GetCustomersByNameUseCase(customerRepository)
+
+    private lateinit var viewModel: CustomerViewModel
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutinesTestRule = CoroutineTestRule()
+
+    @Before
+    fun setup() {
+
+        // Dispatchers.setMain(Dispatchers.Unconfined)
+        Dispatchers.setMain(StandardTestDispatcher())
+        viewModel = CustomerViewModel(
+            getCustomersUseCase,
+            getCustomersByCustomTypeUseCase,
+            getCustomersByNameUseCase,
+
+        )
+        coEvery { getCustomersUseCase.execute() } returns flow {
+            emit(Resource.Success(data = CustomerMock.mockCustomerList()))
+        }
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun ` loading cutomers successfully `() = runTest {
+
+        val customerListState = MutableStateFlow<State<List<Customer>>>(State.Idle)
+        coEvery { getCustomersUseCase.execute() } returns flow {
+            emit(Resource.Success(data = CustomerMock.mockCustomerList()))
+        }
+        viewModel.fetchLatestCustomers()
+        runCurrent()
+        customerListState.value = viewModel.customerListState.value
+
+        assert(customerListState.value is State.Success)
+    }
+}
