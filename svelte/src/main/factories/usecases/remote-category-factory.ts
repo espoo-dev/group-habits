8cc99ffd37e5de9f7ade3a65c3fa69b0fc90f0cc
Iury Nogueira <iunog@hotmail.com>
2023-02-02 14:15:17 -0300

    feat: add pipeline in svelte project :sparkles: (#193)

--- svelte/src/main/factories/usecases/remote-category-factory.ts
+++ svelte/src/main/factories/usecases/remote-category-factory.ts
@@ -7,16 +7,3 @@ export const makeRemoteCategory = (): Categories =>
     'https://group-habits.herokuapp.com/api/v1/categories',
     makeAuthorizeHttpClientDecorator()
   );
-
-// export class makeHttpDecorator implements HttpClient {
-//   constructor(private readonly httpClient: HttpClient) {}
-
-//   async request(data: any): Promise<HttpResponse> {
-//     data.headers = {
-//       ...data.headers,
-//       Authorization:
-//         'Bearer eyJhY2Nlc3MtdG9rZW4iOiJ0Skx3YnNHaXIyRmZQSFdjN29OSVlnIiwidG9rZW4tdHlwZSI6IkJlYXJlciIsImNsaWVudCI6Il9DdTVRWDhmczhYbFlweTZCWW03bXciLCJleHBpcnkiOiIxNzM3OTQxNDg2IiwidWlkIjoidXNlckBlbWFpbC5jb20ifQ==',
-//     };
-//     return await this.httpClient.request(data);
-//   }
-// }
