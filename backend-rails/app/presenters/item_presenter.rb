525580e914bdce5e45f556a22d39f337a8a0efa8
Edimo Silva <edimossilva@gmail.com>
2023-02-11 09:45:24 -0300

    242: change sales_price and purchase_price to float in json

--- backend-rails/app/presenters/item_presenter.rb
+++ backend-rails/app/presenters/item_presenter.rb
@@ -12,8 +12,8 @@ class ItemPresenter < BasePresenter
       id: item.id,
       name: item.name,
       extra_info: item.extra_info,
-      sale_price: item.sale_price,
-      purchase_price: item.purchase_price,
+      sale_price: item.sale_price.to_f,
+      purchase_price: item.purchase_price.to_f,
       item_type: item.item_type,
       category: CategoryPresenter.payload_for_item(item.category),
       sales_unit: SalesUnitPresenter.payload_for_item(item.sales_unit)
