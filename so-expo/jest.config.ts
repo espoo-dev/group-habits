569ef1a1876f6c19c89dbe7459ed1f7dfb3a0c30
leonardobrito <f.leobrito@gmail.com>
2023-02-16 01:08:05 -0300

    :white_check_mark: Update tests files

--- so-expo/jest.config.ts
+++ so-expo/jest.config.ts
@@ -5,14 +5,17 @@ export default {
   coverageProvider: "v8",
   preset: 'jest-expo',
   roots: [
-    "<rootDir>"
+    "<rootDir>/src"
   ],
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)",
     "**/?(*.)+(spec|test).[tj]s?(x)"
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/jest-setup.ts',
+    "@testing-library/jest-native/extend-expect",
+  ],
   "transformIgnorePatterns": [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
   ],
