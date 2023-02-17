44d2d1b93ee4d51122c1629c38d759a37858b786
@Roanderson <roanderson.pinheiro@zig.fun>
2023-02-17 17:03:15 -0300

    test: Created new test endpoint delete item

--- android-so/app/src/test/java/com/group/so/data/services/ItemServiceTest.kt
+++ android-so/app/src/test/java/com/group/so/data/services/ItemServiceTest.kt
@@ -127,15 +127,15 @@ class ItemServiceTest {
             mockWebServer.enqueue(
                 response.setBody(
                     "{\n" +
-                            "    \"id\": 13,\n" +
-                            "    \"name\": \"service teste roanderson\",\n" +
-                            "    \"extra_info\": \"service\",\n" +
-                            "    \"sale_price\": 2000.5,\n" +
-                            "    \"purchase_price\": 0.0,\n" +
-                            "    \"item_type\": \"service\",\n" +
-                            "    \"category\": null,\n" +
-                            "    \"sales_unit\": null\n" +
-                            "}"
+                        "    \"id\": 13,\n" +
+                        "    \"name\": \"service teste roanderson\",\n" +
+                        "    \"extra_info\": \"service\",\n" +
+                        "    \"sale_price\": 2000.5,\n" +
+                        "    \"purchase_price\": 0.0,\n" +
+                        "    \"item_type\": \"service\",\n" +
+                        "    \"category\": null,\n" +
+                        "    \"sales_unit\": null\n" +
+                        "}"
                 )
             )
             service.registerService(
