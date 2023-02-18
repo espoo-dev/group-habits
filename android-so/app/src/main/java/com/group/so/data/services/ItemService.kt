46f25c82a1fa89978bfca227a8df1c6e7f7c3195
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2023-02-17 21:06:38 -0300

    chore: Create new endpoint edit service

--- android-so/app/src/main/java/com/group/so/data/services/ItemService.kt
+++ android-so/app/src/main/java/com/group/so/data/services/ItemService.kt
@@ -1,15 +1,18 @@
 package com.group.so.data.services
 
+
 import com.group.so.data.entities.network.ItemDTO
 import com.group.so.data.entities.request.service.ServiceDataRequest
 import okhttp3.ResponseBody
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.DELETE
+import retrofit2.http.Path
+import retrofit2.http.PUT
+
 
 interface ItemService {
 
@@ -35,4 +38,10 @@ interface ItemService {
 
     @DELETE("items/{id}")
     suspend fun deleteItem(@Path("id") id: Int): Response<ResponseBody>
+
+    @PUT("items/{id}")
+    suspend fun editItem(
+        @Path("id") id: Int,
+        @Body serviceDataRequest: ServiceDataRequest
+    ): ItemDTO
 }
