3bd8b0eb93d49ae54481b328c746d6713475a861
Iury Nogueira <iunog@hotmail.com>
2023-02-19 15:08:07 -0300

    feat: create customer #284 :sparkles: (#305)

--- svelte/src/domain/usecases/customers.ts
+++ svelte/src/domain/usecases/customers.ts
@@ -2,12 +2,21 @@ import type { CustomerModel } from '../models/customer-model';
 
 export interface Customers {
   list: (params?: Customers.Params) => Promise<Customers.Model[]>;
+  create: (payload: Customers.New) => Promise<Customers.Model>;
 }
 
 export namespace Customers {
   export type Params = {
     name?: string;
     customer_type?: string;
+  };
+
+  export type New = {
+    name: string;
+    document_number: number;
+    phone: number;
+    state_inscription: string;
+    customer_type: string;
   };
 
   export type Model = CustomerModel;
