a636baef2630d354ee2a1bfc41d37e71da13a97c
Iury Nogueira <iunog@hotmail.com>
2023-02-19 15:18:06 -0300

    test: add test by type to input component :test_tube: #306 (#308)

--- svelte/src/lib/components/Input/Input.spec.ts
+++ svelte/src/lib/components/Input/Input.spec.ts
@@ -18,25 +18,43 @@ const sut = () => {
 };
 
 describe('Input component', () => {
-  beforeEach(() => {
-    sut();
+  describe('Defaults props', () => {
+    beforeEach(() => {
+      sut();
+    });
+
+    it('should render input', () => {
+      expect(inputElement).toBeInTheDocument();
+    });
+
+    it('should render label', () => {
+      expect(screen.getByText(props.label)).toBeInTheDocument();
+    });
+
+    it('should render label', () => {
+      expect(screen.getByText(props.label)).toBeInTheDocument();
+    });
+
+    it('should change the input value', async () => {
+      const newValue = 'Linkin Park';
+      await user.type(inputElement, newValue);
+      expect(inputElement).toHaveValue(newValue);
+    });
   });
 
-  it('should render input', () => {
-    expect(inputElement).toBeInTheDocument();
-  });
-
-  it('should render label', () => {
-    expect(screen.getByText(props.label)).toBeInTheDocument();
-  });
-
-  it('should render label', () => {
-    expect(screen.getByText(props.label)).toBeInTheDocument();
-  });
-
-  it('should change the input value', async () => {
-    const newValue = 'Linkin Park';
-    await user.type(inputElement, newValue);
-    expect(inputElement).toHaveValue(newValue);
+  describe.only('Check Types', () => {
+    it.each(['text', 'number'])(
+      'should render input with %s',
+      (type: string) => {
+        render(Input, {
+          ...props,
+          type,
+        });
+        expect(screen.getByTestId(`input-${props.label}`)).toHaveAttribute(
+          'type',
+          type
+        );
+      }
+    );
   });
 });
