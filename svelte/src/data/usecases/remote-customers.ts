ca0d6d9c1c65858ded753946eee49f4fb0ed1187
iurynogueira <iunog@hotmail.com>
2023-02-19 14:48:41 -0300

    feat: create customer #284 :sparkles:

--- svelte/src/data/usecases/remote-customers.ts
+++ svelte/src/data/usecases/remote-customers.ts
@@ -1,3 +1,4 @@
+import type { CustomerModel } from 'src/domain/models/customer-model';
 import type { Customers } from 'src/domain/usecases';
 import { HttpResponseHandler } from '../../infra/http/';
 import type { HttpClient } from '../protocols/http';
@@ -13,6 +14,15 @@ export class RemoteCustomer implements Customers {
       url: this.url,
       method: 'get',
       body: params,
+    });
+    return HttpResponseHandler.handleResponse(httpResponse);
+  }
+
+  async create(payload: Customers.New): Promise<CustomerModel> {
+    const httpResponse = await this.httpClient.request({
+      url: this.url,
+      method: 'post',
+      body: payload,
     });
     return HttpResponseHandler.handleResponse(httpResponse);
   }
