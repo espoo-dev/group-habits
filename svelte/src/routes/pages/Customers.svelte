d085f6d466042101f3c4c8c4b40bcc0e54b25519
iurynogueira <iunog@hotmail.com>
2023-02-23 17:05:20 -0300

    refactor: use radiogroup component in new client #325 :hammer:

--- svelte/src/routes/pages/Customers.svelte
+++ svelte/src/routes/pages/Customers.svelte
@@ -6,6 +6,7 @@
   import Menu from '../../lib/components/Menu/Menu.svelte';
   import type { CustomerModel } from 'src/domain/models/customer-model';
   import Input from '../../lib/components/Input/Input.svelte';
+  import RadioGroup from '../../lib/components/RadioGroup/RadioGroup.svelte';
 
   const crudName = 'Cliente'
   let customers = [];
@@ -180,20 +181,13 @@
           />
 
           <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Tipo do cliente</h3>
-          <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                <input id="horizontal-list-radio-license" bind:group={modal.model.customer_type} type="radio" value={'business'} name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Business</label>
-              </div>
-            </li>
-            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                <input id="horizontal-list-radio-id" bind:group={modal.model.customer_type} type="radio" value={'person'} name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                <label for="horizontal-list-radio-id" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Person</label>
-              </div>
-            </li>
-          </ul>
+
+          <RadioGroup options={[
+              {label: 'Business', value: 'business'},
+              {label: 'Person', value: 'person'}
+            ]}  
+            bind:selectedValue={modal.model.customer_type} 
+          />
 
           {#if modal.model.customer_type === 'business'}
             <Input 
