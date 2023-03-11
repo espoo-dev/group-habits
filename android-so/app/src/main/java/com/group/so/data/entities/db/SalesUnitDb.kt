acce8057e65c0689f2e9a4fc7e7798888164d5d8
Roanderson Pinheiro <81362718+roandersonpinheiro@users.noreply.github.com>
2023-03-11 14:27:02 -0300

    Create sales unit dto (#380)
    Co-authored-by: @Roanderson <roanderson.pinheiro@zig.fun>

---
+++ android-so/app/src/main/java/com/group/so/data/entities/db/SalesUnitDb.kt
@@ -0,0 +1,22 @@
+package com.group.so.data.entities.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.group.so.data.entities.model.SalesUnit
+
+@Entity(tableName = "salesUnit")
+data class SalesUnitDb(
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+) {
+    fun toModel(): SalesUnit = SalesUnit(
+        id = id,
+        name = name,
+    )
+}
+
+fun List<SalesUnitDb>.toModel(): List<SalesUnit> =
+    this.map {
+        it.toModel()
+    }
