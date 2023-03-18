62183c71a037ea6dbb20cfc940d07ba038e1dbd4
@Roanderson <roanderson.pinheiro@zig.fun>
2023-03-18 11:48:32 -0300

    chore: Template add new OrderService

--- android-so/app/src/main/java/com/group/so/presentation/ui/serviceOrder/model/TabItem.kt
+++ android-so/app/src/main/java/com/group/so/presentation/ui/serviceOrder/model/TabItem.kt
@@ -2,13 +2,12 @@ package com.group.so.presentation.ui.serviceOrder.model
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import com.group.so.R
-import com.group.so.presentation.ui.serviceOrder.ItemsScreen
-import com.group.so.presentation.ui.serviceOrder.SummaryScreen
+
 
 typealias ComposableFun = @Composable () -> Unit
 
 sealed class TabItem(@StringRes val title: Int, var screen: ComposableFun) {
 
-    object Resumed : TabItem(R.string.tabs_order_service_summary, { SummaryScreen() })
-    object Items : TabItem(R.string.tabs_order_service_items, { ItemsScreen() })
+    object Resumed : TabItem(R.string.tabs_order_service_summary, { })
+    object Items : TabItem(R.string.tabs_order_service_items, {  })
 }
