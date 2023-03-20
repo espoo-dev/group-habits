3d9557ae65c9d3ed01633f88e9ba5e080bcc061f
Iury Nogueira <iunog@hotmail.com>
2023-03-20 20:12:33 -0300

    style: improve select style :art: (#401)

--- svelte/src/lib/components/Select/Select.svelte
+++ svelte/src/lib/components/Select/Select.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onMount } from 'svelte';
 
   interface Option {
     name: string;
@@ -11,32 +11,34 @@
   export let selected: string | number;
   export let service: { factory: any } = null;
   export let valueKey: string = 'id';
-  
+
   const onLoad = async () => {
-    options = await service.factory.list()
-  }
+    options = await service.factory.list();
+  };
 
   onMount(() => {
     if (service) {
-      onLoad()
+      onLoad();
     }
-  })
+  });
 </script>
 
-<label for="select" class="block mb-2 text-sm font-medium text-gray-900"
-  >{label || 'Selecione uma opção'}</label
->
-<select
-  data-testid={`${label}-select-id`}
-  bind:value={selected}
-  id="select"
-  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
->
-  {#if options.length}
-    {#each options as option}
-      <option value={option[valueKey]} selected={option.value === selected}
-        >{option.name}</option
-      >
-    {/each}
-  {/if}
-</select>
+<div>
+  <label for="select" class="block mb-2 text-sm font-medium text-gray-900">
+    {label || 'Selecione uma opção'}
+  </label>
+  <select
+    data-testid={`${label}-select-id`}
+    bind:value={selected}
+    id="select"
+    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+  >
+    {#if options.length}
+      {#each options as option}
+        <option value={option[valueKey]} selected={option.value === selected}
+          >{option.name}</option
+        >
+      {/each}
+    {/if}
+  </select>
+</div>
