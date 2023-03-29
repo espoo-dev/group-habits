12e40c12488307628576f3950d19cc0acfffa3b1
@Roanderson <roanderson.pinheiro@zig.fun>
2023-03-29 14:15:56 -0300

    chore: Added date field in work order creation

--- android-so/app/src/main/java/com/group/so/presentation/ui/serviceOrder/components/ServiceOrderContent.kt
+++ android-so/app/src/main/java/com/group/so/presentation/ui/serviceOrder/components/ServiceOrderContent.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
--- android-so/app/src/main/java/com/group/so/presentation/ui/serviceOrder/components/ServiceOrderFormContent.kt
+++ android-so/app/src/main/java/com/group/so/presentation/ui/serviceOrder/components/ServiceOrderFormContent.kt
@@ -9,18 +9,31 @@
 package com.group.so.presentation.ui.serviceOrder.components
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.group.so.R
 import com.group.so.core.State
 import com.group.so.core.presentation.components.AsyncData
+import com.group.so.core.presentation.components.fields.DateField
+import com.group.so.core.presentation.components.fields.DatePicker
+import com.group.so.core.presentation.components.fields.MyTextField
 import com.group.so.core.presentation.components.generic.GenericError
+import com.group.so.core.presentation.components.validations.TextState
 import com.group.so.data.entities.model.Customer
 
 @Composable
@@ -29,24 +42,65 @@ fun ServiceOrderFormContent(
     customerListState: State<List<Customer>>
 ) {
     var customer by remember { mutableStateOf(0) }
+    val creationDate = remember { TextState() }
+    var showPicker by remember { mutableStateOf(false) }
 
-    Box(Modifier.padding()) {
-        AsyncData(resultState = customerListState, errorContent = {
-            GenericError(onDismissAction = {})
-        }) { customerList ->
-            customerList?.let { customers ->
-                customer = customers.first().id
-                if (customers.isEmpty()) {
-                    Text("")
-                } else {
-                    AutoCompleteCustomer(
-                        customers = customerList,
-                        itemSelected = { itemSelected ->
-                            customer = itemSelected.id
-                        },
-                    )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(Modifier.padding()) {
+            AsyncData(resultState = customerListState, errorContent = {
+                GenericError(onDismissAction = {})
+            }) { customerList ->
+                customerList?.let { customers ->
+                    customer = customers.first().id
+                    if (customers.isEmpty()) {
+                        Text("")
+                    } else {
+                        AutoCompleteCustomer(
+                            customers = customerList,
+                            itemSelected = { itemSelected ->
+                                customer = itemSelected.id
+                            },
+                        )
+                    }
                 }
             }
+
         }
+
+        if (showPicker)
+            DatePicker(
+                title = R.string.label_title_calendar,
+                onDateSelected = { formattedDate ->
+                    creationDate.text = formattedDate
+                },
+                onDismissRequest = {
+                    showPicker = false
+                })
+
+        DateField(
+            labelText = stringResource(R.string.lbl_text_date_creation),
+            textColor = MaterialTheme.colors.primary,
+            valueText = creationDate.text,
+            error = creationDate.error,
+            iconColor = MaterialTheme.colors.primary,
+            iconClear = creationDate.text.isNotBlank(),
+            clearText = {
+                creationDate.text = ""
+            },
+            onClick = {
+                showPicker = true
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            creationDate.text = it
+            creationDate.validate()
+        }
+
     }
+
 }
